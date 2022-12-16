116ddcc08727e0d197ea89bb2fa7fd9e9e3ed231
Valepa2911 <megp17102017@mail.ru>
2022-12-16 20:33:38 +0400

    сохран.

---
+++ 222222222/Program.cs
@@ -0,0 +1,41 @@
+﻿//Задайте прямоугольный двумерный массив. Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+//(можно использовать готовую функцию)
+//Например, задан массив:
+//1 4 7 2
+//5 9 2 3
+//8 4 2 4
+//5 2 6 7
+//Программа считает сумму элементов в каждой строке и выдаёт номер строки с наименьшей суммой элементов: 1 строка
+Console.WriteLine("Введите число(кол-во строк массива)");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число(кол-во столбцов массива)");
+int n = Convert.ToInt32(Console.ReadLine());
+int[,] array2d = new int[m, n];
+Random random = new Random();
+
+for (int i = 0; i < array2d.GetLength(0); i++)
+{
+    for (int j = 0; j < array2d.GetLength(1); j++)
+
+    {
+        array2d[i, j] = new Random().Next(0, 20);
+        Console.Write($"{array2d[i, j]}" + " ");
+    }
+    Console.WriteLine();
+}
+
+int MinSum(int[,] array2d)
+{
+    int minRowSum = int.MaxValue;
+    for (int i = 0; i < m; i++)
+    {
+        int result = 0;
+        for (int j = 0; j < n; j++)
+            result += array2d[i, j];
+        if (result < minRowSum)
+            minRowSum = result;
+    }
+    return minRowSum;
+}
+Console.WriteLine($"Сумма:{MinSum(array2d)}");
+
---
+++ Zadacha56/Program.cs
@@ -0,0 +1,48 @@
+﻿//Задайте прямоугольный двумерный массив. Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+//(можно использовать готовую функцию)
+//Например, задан массив:
+//1 4 7 2
+//5 9 2 3
+//8 4 2 4
+//5 2 6 7
+//Программа считает сумму элементов в каждой строке и выдаёт номер строки с наименьшей суммой элементов: 1 строка
+
+int n1, n2;
+int[,] A;
+
+Console.Write("Введите количество строк в матрице: ");
+n1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите количество столбцов в матрице: ");
+n2 = Convert.ToInt32(Console.ReadLine());
+A = new int[n1, n2];
+Random rnd = new Random();
+
+// Присвоение значений датчиком случайных чисел
+for (int i = 0; i < n1; i++)
+    for (int j = 0; j < n2; j++)
+        A[i, j] = rnd.Next(-0, 10 + 1);
+
+//Вывод массива
+for (int i = 0; i < n1; i++, Console.WriteLine())
+    for (int j = 0; j < n2; j++)
+        Console.Write(A[i, j] + "\t");
+int minRowSum = int.MaxValue, indexMinRow = 0;
+
+for (int i = 0; i < n1; i++)
+{
+    int rowSum = 0;
+    for (int j = 0; j < n2; j++)
+        rowSum += A[i, j];
+
+    if (rowSum < minRowSum)
+    {
+        minRowSum = rowSum;
+        indexMinRow = i;
+    }
+}
+
+Console.WriteLine("Строка с минимальной суммой элементов");
+for (int j = 0; j < n2; j++)
+    Console.Write(A[indexMinRow, j] + " ");
+    Console.WriteLine();
+    Console.WriteLine($"Сумма - {minRowSum}");
